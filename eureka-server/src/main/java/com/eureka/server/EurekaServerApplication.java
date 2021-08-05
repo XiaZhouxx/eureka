dc0c6176dffe8151229077da740b05378ba974e6
xiazhou <xiazhou@wemew.cn>
2021-08-05 16:54:33 +0800

    eureka

---
+++ eureka-server/src/main/java/com/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xz
+ * @ClassName EurekaServerApplication
+ * @Description
+ * @date 2021/8/5 9:55
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
